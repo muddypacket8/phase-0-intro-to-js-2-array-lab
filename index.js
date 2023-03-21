fcb0282d9c428a16a6e45f162eb454b5c6a572c2
collins <collinsleonkimani@outlook.com>
2023-03-22 00:45:19 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution here!
+describe("index.js", () => {
+    let cats = ["Milo", "Otis", "Garfield"];
+  
+    beforeEach(() => {
+      cats = ["Milo", "Otis", "Garfield"];
+    });
+  
+    describe("cats", () => {
+      it("is assigned an initial value of ['Milo', 'Otis', 'Garfield']", () => {
+        expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
+      });
+    });
+  
+    describe("Array functions", () => {
+      describe("destructivelyAppendCat(name)", () => {
+        it("appends a cat to the end of the cats array", () => {
+          destructivelyAppendCat("Ralph");
+          expect(cats).to.have.ordered.members([
+            "Milo",
+            "Otis",
+            "Garfield",
+            "Ralph",
+          ]);
+        });
+      });
+    });
+  });
+  
